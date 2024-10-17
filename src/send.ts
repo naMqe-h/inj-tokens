094c4b3d4a9d700a57e61f18c0453cdad2624d03
naMqe <namqe07@gmail.com>
2024-10-17 10:40:31 +0200

    transaction sending module has been modified

--- src/send.ts
+++ src/send.ts
@@ -1,23 +1,38 @@
 import { MsgBroadcasterWithPk, MsgSend } from '@injectivelabs/sdk-ts'
 import { Network } from '@injectivelabs/networks';
-import { BigNumberInBase } from '@injectivelabs/utils';
 
-const injectiveAddress = ''
-const privateKey = '';
+const wallets = [
+    {
+        publicKey: "",
+        privateKey: "",
+    }
+]
+
+const ONE_INJ = "1000000000000000000"
+const AMOUNT = 1
+
+const send = async (publicKey: string, privateKey: string) => {
+    try {
+        const txHash = await new MsgBroadcasterWithPk({ privateKey: privateKey, network: Network.MainnetSentry }).broadcast({
+            msgs: MsgSend.fromJSON({
+                amount: {
+                    amount: (+ONE_INJ * AMOUNT).toString(),
+                    denom: 'inj',
+                },
+                dstInjectiveAddress: "",
+                srcInjectiveAddress: publicKey,
+            }),
+        });
+        console.log(txHash);
+    } catch (e) {
+        console.log(e)
+    }
+}
 
 const main = async () => {
-    const txHash = await new MsgBroadcasterWithPk({ privateKey, network: Network.MainnetSentry }).broadcast({
-        msgs: MsgSend.fromJSON({
-            amount: {
-                amount: new BigNumberInBase(0).toFixed(),
-                denom: '',
-            },
-            dstInjectiveAddress: "",
-            srcInjectiveAddress: injectiveAddress,
-        }),
-    });
-    
-    console.log(txHash);
+    for (let i = 0; i < wallets.length; i++) {
+        await send(wallets[i].publicKey, wallets[i].privateKey)
+    }
 }
 
 main()
